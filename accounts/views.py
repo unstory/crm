4a3c3352a40a9095c4c853430692ce246a9b12f4
unstory <2075218138@qq.com>
2020-09-10 17:06:16 +0800

    user customer onetoone

--- accounts/views.py
+++ accounts/views.py
@@ -69,9 +69,20 @@ def home(request):
 
 	return render(request, 'accounts/dashboard.html', context)
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-    context = {}
-    return render(request, "accounts/user.html", context)
+	orders = request.user.customer.order_set.all()
+
+	total_orders = orders.count()
+	delivered = orders.filter(status='Delivered').count()
+	pending = orders.filter(status='Pending').count()
+
+	print('ORDERS:', orders)
+
+	context = {'orders':orders, 'total_orders':total_orders,
+	'delivered':delivered,'pending':pending}
+	return render(request, 'accounts/user.html', context)
 
 @login_required(login_url="login")
 @allowed_users(allowed_roles=['admin'])
